9e6b0b2a4f956d80faa0433e4235c34051e7c2fe
Leonardo <potty3800@gmail.com>
2022-03-25 15:17:31 -0400

    Filled with personal information. Commented off ContactForm

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,11 +1,11 @@
 <script>
 import feather from 'feather-icons';
-import ContactForm from '@/components/contact/ContactForm.vue';
+// import ContactForm from '@/components/contact/ContactForm.vue';
 import ContactDetails from '@/components/contact/ContactDetails.vue';
 
 export default {
 	components: {
-		ContactForm,
+		// ContactForm,
 		ContactDetails,
 	},
 	data: () => {
@@ -13,17 +13,17 @@ export default {
 			contacts: [
 				{
 					id: 1,
-					name: 'Your Address, Your City, Your Country',
+					name: '1st Street 15 Virginia, Santa Clara City, Cuba',
 					icon: 'map-pin',
 				},
 				{
 					id: 2,
-					name: 'email@domain.com',
+					name: 'potty3800@gmail.com',
 					icon: 'mail',
 				},
 				{
 					id: 3,
-					name: '555 8888 888',
+					name: '(+53) 53 745850',
 					icon: 'phone',
 				},
 			],
@@ -41,10 +41,13 @@ export default {
 
 <template>
 	<div
-		class="container mx-auto flex flex-col-reverse md:flex-row py-5 md:py-10 md:mt-10"
+		class="container mx-auto flex flex-col md:flex-row py-5 md:py-10 md:mt-10"
 	>
 		<!-- Contact form -->
-		<ContactForm />
+		<!-- <ContactForm /> -->
+		
+		<!-- Contact Image -->
+		<img class="pt-2 mt-5 md:mt-0 w-full md:w-2/5" src="../assets/images/contact.svg" alt="contact-ilustation">
 
 		<!-- Contact details -->
 		<ContactDetails :contacts="contacts" />
